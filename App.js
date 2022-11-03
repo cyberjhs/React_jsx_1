ff2b18d1fe9c224ede416fe61f6ce9baf22f54d8
Navneet Vishwakarma <68511498+cyberjhs@users.noreply.github.com>
2022-11-03 11:52:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+    <div className="about-Text">
+      <h1>About Us</h1>
+      <br></br>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, aperiam maiores. Adipisci, et? Beatae nam et.</p>
+      <br></br>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo corrupti, earum pariatur mollitia perferendis facilis ut illum ipsam cum facere! Ipsa eos aperiam amet repellendus deleniti? Voluptas fuga doloribus architecto deleniti, incidunt officia facere, officiis neque itaque vero ullam sint, blanditiis accusantium earum. Maiores est tenetur beatae saepe doloribus libero atque quaerat sit officia a ipsum natus, maxime corporis, facere iusto esse odio fuga eius eum? Quae facere provident quisquam!</p>
+      <br></br>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit dolorem ex aliquam sint, commodi illum?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit dolorem ex aliquam sint, commodi illum_________.</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
